a91315c3e1e6028493e2bcba551310f0b939397f
vinhnguyen <nhquocvinh@gmail.com>
2025-03-31 23:41:59 +0700

    chore: fix pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'bmgd-landing',
     script: 'bun run start',
-    interpreter: 'bash',
+    interpreter: '/usr/bin/env',
     env: {
       NODE_ENV: 'production',
       PORT: 3100
